0a6f0321589a395ddbf1e0f137ddd9fe19d0a72d
João da Mata <jlfilho@uea.edu.br>
2022-08-24 14:13:26 -0400

    Primeiro commit

---
+++ src/app/Programa1.java
@@ -0,0 +1,15 @@
+package app;
+
+public class Programa1 {
+
+	public static void main(String[] args) {
+		int a, b;
+		double resultado;
+		a = 5;
+		b = 2;
+		resultado = (double) a / b;
+		System.out.println(resultado);
+
+	}
+
+}
